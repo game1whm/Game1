a783367ed30d49ddca420f737e5158fbd364d60d
MKPiddi <nils@dittnet.de>
2019-05-19 14:29:54 +0200

    Scripts added
    Ballsteuerung und Kamera

---
+++ Assets/Scripts/Ballsteuerung.cs
@@ -0,0 +1,62 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Ballsteuerung : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float boostforce = 30.0f;
+    Rigidbody rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Drehen
+        float turn = Input.GetAxis("Horizontal");
+        transform.Rotate(0.0f, turn, 0.0f, Space.Self);
+        // Vorwärts/Rückwärts
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.z = Input.GetAxisRaw("Vertical");
+        rBody.AddRelativeForce(controlSignal * speed);
+
+        //Boost
+        if (Input.GetKeyDown("b"))
+        {
+            Vector3 boost = Vector3.zero;
+            boost.z = Input.GetAxisRaw("Vertical");
+            rBody.AddRelativeForce(boost * speed * boostforce);
+        }
+
+        // Restart einleiten
+        if (Input.GetKey("r"))
+        {
+            Restart();
+        }
+
+        if (transform.position.y < -1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        //Quit Game
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+    //Restart
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
---
+++ Assets/Scripts/Kameraverfolgt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kameraverfolgt : MonoBehaviour
+{
+    public GameObject player;
+    public float distanceBehind = 2f;
+    public float heightAbove = 3f;
+    public float lookAtBefore = 2f;
+    Vector3 playerPrevPos, playerMoveDir;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerPrevPos = player.transform.position;
+    }
+    void LateUpdate()
+    {
+        playerMoveDir = player.transform.position - playerPrevPos;
+        if (playerMoveDir != Vector3.zero)
+        {
+            playerMoveDir.Normalize();
+            playerMoveDir.Normalize();
+            Vector3 newPos = player.transform.position - playerMoveDir * distanceBehind;
+            Vector3 newLookat = player.transform.position + playerMoveDir * lookAtBefore;
+            newPos.y += heightAbove; // required height
+            transform.position = newPos;
+            // where to look
+
+            transform.LookAt(newLookat);
+
+            playerPrevPos = player.transform.position;
+        }
+    }
+}
