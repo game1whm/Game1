5ca5fe346be5ff814fb939c0e144d13c78a0004d
MKPiddi <nils@dittnet.de>
2019-05-22 21:04:27 +0200

    Kamera verbessert

--- Assets/Scripts/Kameraverfolgt.cs
+++ Assets/Scripts/Kameraverfolgt.cs
@@ -7,8 +7,10 @@ public class Kameraverfolgt : MonoBehaviour
     public GameObject player;
     public float distanceBehind = 2f;
     public float heightAbove = 3f;
-    public float lookAtBefore = 2f;
-    Vector3 playerPrevPos, playerMoveDir;
+    public float lookAtBefore = 3f;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 playerPrevPos, playerMoveDir;
 
     // Use this for initialization
     void Start()
@@ -25,12 +27,15 @@ public class Kameraverfolgt : MonoBehaviour
             Vector3 newPos = player.transform.position - playerMoveDir * distanceBehind;
             Vector3 newLookat = player.transform.position + playerMoveDir * lookAtBefore;
             newPos.y += heightAbove; // required height
-            transform.position = newPos;
+
+            //transform.position = newPos;
+            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
             // where to look
 
             transform.LookAt(newLookat);
 
             playerPrevPos = player.transform.position;
+
         }
     }
 }
